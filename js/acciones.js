e3836ad90b38657059040d162736665a8915ed09
jacqueline orzuna cortes <jacquelineorzuna95@gmail.com>
2019-05-10 09:14:31 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -1,26 +1,26 @@
-//JavaScript Document
+//JavaScript 
 
 $(document).ready(function(e) {
-document.addEventListener("deviceready", function(){
+    document.addEventListener("deviceready",function(){
 		
-$('#izquierda').on("swipeleft", function(){
-	navigator.notification. alert("Deslizo a la izquierda", function(){"Aplicacion ","Aceptar"});
+		$('#izquierda').on("swipeleft",function(){
+			navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion ","Aceptar"});			
 });
 
-$('#derecha').on("swipeleft", function(){
-	navigator .notification. confirm("Que quieres hacer?", function(opt){
-		switch(opt)
-	{
-		case 1:
-		navigator.notification.beep(1);
-		break;
+$('#derecha').on("swiperight",function(){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
 		
-		case 2:
-		navigator.notification.vibrate(1000);
-		break;
-	}
+switch(opt)
+{ 
+case 1:
+navigator.notification.beep(1)
+break;
+case 2: 
+navigator.notification.vibrate(1000);
+break;
+}
 	},"Aplicacion ","Beep,Vibrar,Cancelar");
 	
 });
-},false);
+	},false);
 });
